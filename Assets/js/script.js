c5f8c41445c8d72a6daba05178a5e8c40a9a1166
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-12 16:50:36 -0400

    Template for JavaScript

---
+++ Assets/js/script.js
@@ -0,0 +1,30 @@
+// Assignment code
+
+// individual arrays to include the password criteria set.
+
+// function that generates a random number upto a maximum value.
+  // var value = Math.floor(Math.random() * (max-min+1)+min);
+
+// function to get password lenght
+  // ask the user for a password length
+
+  // check if the prompt meets the password criteria 
+
+// Get references to the #generate element
+
+// prompts the user to select the password length and criteria
+    //request password length
+    //check password criteria:lowercase, uppercase, numbers and special characters
+    // declare an array to store the selected password characters
+    // contatenate arrays to include the selected criteria in one array
+    // validate that atleast one character type is selected.
+
+// select random characters from the contatenated array to generate a unique password
+// loop through the password lenght
+    // select a random number between 0 and char array lenght
+    // select the character from concatenated array and store it to the passwordArray
+ // return the password array without the commas using .join("")
+
+// Write password to the #password input
+
+// Add event listener to generate button
