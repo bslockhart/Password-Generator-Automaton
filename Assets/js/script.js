4a6bc183db216b4263d9d73845b72835fd044ea2
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-12 16:52:14 -0400

    Array Section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,12 @@
 // Assignment code
+var passwordArray = [];
 
 // individual arrays to include the password criteria set.
+var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberArray = ["1","2","3","4","5","6","7","8","9","0"];
+var spCharArray = [" ","!","'","#","$","%","&","\"",")","(","*","+",",","-",".","/",":",";",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+
 
 // function that generates a random number upto a maximum value.
   // var value = Math.floor(Math.random() * (max-min+1)+min);
